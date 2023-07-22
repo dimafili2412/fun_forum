481eca0650f9121b4a7e7e5ed0543e659643169a
dimafili2412 <dimafili2412@gmail.com>
2023-07-22 10:22:42 +0300

    express template + basic logger

---
+++ server/logger/logger.js
@@ -0,0 +1,10 @@
+const log = {
+    info: (message) => {
+        console.log(message);
+    },
+    error: (message) => {
+        console.error(message);
+    },
+};
+
+module.exports = log;
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const log = require('./logger/logger');
+
+const httpPort = process.env.HTTP_PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Test!');
+});
+
+app.listen(httpPort, () => {
+    log.info(`Server is running on port ${httpPort}`);
+});
